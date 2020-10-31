d0d491adbbc1ba2b52ba7735d2a66d35b6c6d318
Hartman, Ondrej <Bc.Hartman@ncr.com>
2020-10-31 13:18:52 +0100

    Initial commit

---
+++ Assets/Scripts/MainMenuActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuActions : MonoBehaviour
+{
+    public void NewGame()
+    {
+
+    }
+
+    public void Continue()
+    {
+
+    }
+
+    public void ShowSettings()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
